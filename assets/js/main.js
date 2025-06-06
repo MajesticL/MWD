012e3743ed4f79797f7fdf9f437acf95af856ab5
DalilahH1 <dalilahheyward@gmail.com>
2025-06-05 20:36:21 -0400

    retry commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     gsap.to(".coming", {
+//             rotation: 360,
+//             duration: 1,
+//         });
+// });
